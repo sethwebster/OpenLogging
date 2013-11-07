43b3ec5d6c885f8100b56943dbff6df040c73a4a
Seth Webster <sethwebster@gmail.com>
2013-11-07 00:44:58 -0500

    Added required to log message

--- SethWebster.OpenLogging/Models/LogMessage.cs
+++ SethWebster.OpenLogging/Models/LogMessage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -13,12 +14,16 @@ namespace SethWebster.OpenLogging.Models
             this.DateOfEvent = DateTimeOffset.Now;
         }
         public int LogMessageId { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public string Message { get; set; }
         public string Body { get; set; }
         public DateTimeOffset DateCreated { get; set; }
         public DateTimeOffset DateOfEvent { get; set; }
+        [Required]
         public Client Client { get; set; }
     }
 }
