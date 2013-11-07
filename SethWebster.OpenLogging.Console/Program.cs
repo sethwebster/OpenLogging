24999926caa09c32eee5f33d1820e0429077f20c
Seth Webster <sethwebster@gmail.com>
2013-11-07 00:42:27 -0500

    Refactored a bit

--- SethWebster.OpenLogging.Console/Program.cs
+++ SethWebster.OpenLogging.Console/Program.cs
@@ -22,22 +22,10 @@ namespace SethWebster.OpenLogging.Console
             // Uncomment this line when not using Fiddler
             // uri = "http://localhost:6=757/api/";
             OpenLoggingClient cl = new Client.OpenLoggingClient(new Uri(uri));
-            Writeline("Using " + uri + " as endpoint for operations");
-            Writeline("Creating Client...");
-            var clientCreationResult = await cl.CreateClient(new SethWebster.OpenLogging.Models.Client() { ClientName = "Southwest" });
-            Writeline("Created Client #" + clientCreationResult.ClientId);
-            Writeline("Creating Log Message Entry ...");
-
-            cl = new OpenLoggingClient(clientCreationResult.CurrentApiKey, new Uri(uri));
-
-            var logRes = await cl.NewLogEntry(new LogMessage()
-            {
-                Title = "New Log MEssage" + DateTime.Now,
-                Category = "ERROR",
-                Body = "Body of " + DateTime.Now,
-                Message = "This is the dang message"
-            });
-            return logRes;
+            var clientCreationResult = await CreateClient(uri, cl);
+            var logCreationResult = await CreateLogEntry(uri, clientCreationResult);
+            var deleteClientResult = await DeleteClient(clientCreationResult, cl);
+            return logCreationResult;
             //Writeline("Press ENTER to start.");
             //System.Console.ReadLine();
             //System.Console.WriteLine("Server is located at " + uri);
@@ -93,6 +81,37 @@ namespace SethWebster.OpenLogging.Console
 
             //System.Console.ReadLine();
 
+        }
+
+        private static async Task<Models.Client> DeleteClient(Models.Client client, OpenLoggingClient cl)
+        {
+            Writeline("Deleting Client #" + client.ClientId);
+            var res = await cl.DeleteClient(client);
+            return res;
+        }
+
+        private static async Task<LogMessage> CreateLogEntry(string uri, Models.Client clientCreationResult)
+        {
+            Writeline("Creating Log Message Entry ...");
+            var cl2 = new OpenLoggingClient(clientCreationResult.CurrentApiKey, new Uri(uri));
+
+            var logRes = await cl2.NewLogEntry(new LogMessage()
+            {
+                Title = "New Log MEssage" + DateTime.Now,
+                Category = "ERROR",
+                Body = "Body of " + DateTime.Now,
+                Message = "This is the dang message"
+            });
+            return logRes;
+        }
+
+        private static async Task<Models.Client> CreateClient(string uri, OpenLoggingClient cl)
+        {
+            Writeline("Using " + uri + " as endpoint for operations");
+            Writeline("Creating Client...");
+            var clientCreationResult = await cl.CreateClient(new SethWebster.OpenLogging.Models.Client() { ClientName = "Southwest" });
+            Writeline("Created Client #" + clientCreationResult.ClientId);
+            return clientCreationResult;
         }
         static void Main(string[] args)
         {
